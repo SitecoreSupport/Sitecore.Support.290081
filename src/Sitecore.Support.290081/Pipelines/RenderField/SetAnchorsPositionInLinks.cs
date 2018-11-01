408077e8d235485f44e823cce99ee77f6bcd0a2c
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2018-11-01 11:44:46 +0200

    Initial commit + import the source code

---
+++ src/Sitecore.Support.290081/Pipelines/RenderField/SetAnchorsPositionInLinks.cs
@@ -0,0 +1,62 @@
+﻿namespace Sitecore.Support.Pipelines.RenderField
+{
+  using Sitecore.Diagnostics;
+  using System;
+  using System.Text;
+  using Sitecore.Pipelines.RenderField;
+
+  public class SetAnchorsPositionInLinks : Sitecore.Pipelines.RenderField.SetAnchorsPositionInLinks
+  {
+    #region Public Methods
+
+    /// <summary>Sets the correct position of anchors in RTE links.</summary>
+    /// <param name="args">The arguments.</param>
+    /// <contract>
+    ///   <requires name="args" condition="none"/>
+    /// </contract>
+    public virtual new void Process([NotNull] RenderFieldArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (args.FieldTypeKey == "rich text")
+      {
+        args.Result.FirstPart = this.CheckLinks(args.Result.FirstPart);
+        args.Result.LastPart = this.CheckLinks(args.Result.LastPart);
+      }
+    }
+
+    #endregion
+
+    #region Protected Methods
+
+    /// <summary>
+    /// Checks if links exist in the RTE field and modifies them.
+    /// </summary>
+    /// <param name="text">The text.</param>
+    protected virtual new string CheckLinks(string text)
+    {
+      if (!text.Contains(pattern))
+      {
+        return text;
+      }
+
+      int indexOfPattern = 0;
+      int indexOfUrlClosingQuote = 0;
+      StringBuilder result = new StringBuilder();
+      while ((indexOfPattern = text.IndexOf(pattern, indexOfUrlClosingQuote, StringComparison.Ordinal)) >= 0)
+      {
+        var lengthOfTextWithoutUrl = indexOfPattern - indexOfUrlClosingQuote + pattern.Length;
+        result.Append(text.Substring(indexOfUrlClosingQuote, lengthOfTextWithoutUrl));
+        var lengthOfUrl = text.IndexOf("\"", indexOfPattern + pattern.Length + 1, StringComparison.Ordinal) - indexOfPattern - pattern.Length;
+        string urlFromPattern = text.Substring(indexOfPattern + pattern.Length, lengthOfUrl);
+        urlFromPattern = this.MoveAnchor(urlFromPattern);
+        result.Append(urlFromPattern);
+        indexOfUrlClosingQuote = indexOfPattern + pattern.Length + urlFromPattern.Length;
+      }
+
+      result.Append(text.Substring(indexOfUrlClosingQuote));
+
+      return result.ToString();
+    }
+    #endregion
+  }
+}
---
+++ src/Sitecore.Support.290081/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.290081")]
+[assembly: AssemblyProduct("Sitecore.Support.290081")]
+[assembly: ComVisible(false)]
